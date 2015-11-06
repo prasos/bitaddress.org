de2222fa9a0918a2e8cd1ac6df5826ed369058f9
Joel Lehtonen <jlehtonen@prasos.fi>
2015-11-06 16:18:58 +0200

    Add support for P2SH address validation #370

--- src/pubkey_validator.js
+++ src/pubkey_validator.js
@@ -5,13 +5,29 @@ function validatePubKey(pubKeyHex, userAddress) {
     return address == userAddress;
 }
 
+// Validates given bitcoin address. Code derived from
+// Bitcoin.Address.decodeString and added P2SH support
 function validateAddress(addr) {
     try {
-	Bitcoin.Address.decodeString(addr);
-	return true;
+	var bytes = Bitcoin.Base58.decode(addr);
     } catch (e) {
+	return false; // Invalid character
+    }
+    var hash = bytes.slice(0, 21);
+    var checksum = Crypto.SHA256(Crypto.SHA256(hash, { asBytes: true }), { asBytes: true });
+
+    if (checksum[0] != bytes[21] ||
+	checksum[1] != bytes[22] ||
+	checksum[2] != bytes[23] ||
+	checksum[3] != bytes[24]) {
 	return false;
     }
+
+    var version = hash.shift();
+
+    if (version == 0) return true; // P2PKH address
+    if (version == 5) return true; // P2SH address
+    return false; // Unknown
 }
 
 window['validatePubKey'] = validatePubKey;
